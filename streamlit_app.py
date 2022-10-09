d29d5cf962b62b6d79cab909d8145767d9f0be8d
msaid.eth <108574408+0xmsu@users.noreply.github.com>
2022-10-09 11:48:31 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,12 +7,9 @@ import streamlit as st
 """
 # Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+What we need:
+- For the top 3 Uniswap v3 pools, historical data for the different concentrated liquidity ranges:
+this is, for each tick, the amount of asset locked by pair and their price.
 """
 
 
@@ -36,3 +33,18 @@ with st.echo(code_location='below'):
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
+
+
+
+"""
+## My thoughts:
+*   I would use AWS S3 to store the data. It is a cheap and reliable storage solution.
+*   I also would use AWS Redshift mainly because it is a columnar database and it is optimized for analytical workloads. It is also easy to scale. 
+*   For orchestration, I would use Airflow. It is a great tool for scheduling and monitoring workflows. It is also easy to integrate with other AWS services. And job can run on the Elastic Container Service (ECS) or Elastic Kubernetes Service. Therefore, it is easy to scale up/out.
+*   For realtime data processing, I would use AWS Kinesis. It is a fully managed service that can easily scale up/out. It is also easy to integrate with other AWS services such as AWS Redshift, Kinesis firehose or lambda.
+*   For visualization, I would use Metabase. It is a great tool for data visualization.
+*   In crypto, There are several different tools that can be used to extract data from the blockchain. For example, Infura, Etherscan, The Graph, etc. I would use The Graph because it is a great tool for querying the blockchain. 
+"""
+
+"""
+## My code: https://github.com/0xmsu/streamlit-uniswap"""
